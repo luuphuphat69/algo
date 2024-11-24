932b7dd05765ff4a47760ca961cec70ecb36eb56
luuphuphat69 <luuphuphat69@gmail.com>
2024-11-24 13:39:05 +0700

    roman_to_integer

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-import initTwoSum from './twosum/twosum.js';
-import palindrome_number from './palindrome number/palindrome_number.js';
+//import initTwoSum from './twosum/twosum.js';
+//import palindrome_number from './palindrome number/palindrome_number.js';
+import initRomanToInteger from "./roman to integer/roman_to_integer.js";
 const app = () =>{
     //initTwoSum();
-    palindrome_number(1000021);
+    //palindrome_number(1000021);
+    initRomanToInteger('MCMXCIV');
 }
 app();
